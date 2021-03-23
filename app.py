8b216afcde34aa8d202d61b8b043391c007140dd
John L <johnny@example.com>
2021-03-22 20:35:07 -0500

    added logic for calc function in app.py, edited README.txt to .md, created form and number inputs for calculator in index along with some bootstrap styles

--- app.py
+++ app.py
@@ -6,3 +6,31 @@ app = Flask(__name__)
 @app.route('/')
 def home_page():
     return render_template('index.html')
+
+""" Form submission route """
+@app.route('/send', methods=['POST'])
+def send(sum=sum):
+    if request.method == 'POST':
+        # Start pulling data from Form input
+        num1 = request.form['num1']
+        num2 = request.form['num2']
+        operation = request.form['operation']
+        # Calculation IF Statements
+        if operation == 'add':
+            sum = float(num1) + float(num2)
+            return render_template('index.html', sum=sum) # basically passing in a prop like in Vue
+        elif operation == 'subtract':
+            sum = float(num1) - float(num2)
+            return render_template('index.html', sum=sum)
+        elif operation == 'multiply':
+            sum = float(num1) * float(num2)
+            return render_template('index.html', sum=sum)
+        elif operation == 'divide':
+            sum = float(num1) / float(num2)
+            return render_template('index.html', sum=sum)
+        else:
+            return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True) # Turned on developer mode, shows us actual errors when they pop up
